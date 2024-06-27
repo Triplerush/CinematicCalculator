6ca582768075c3dede3ed1a4484d9226fadc2094
Diego Huamani <dhuamanilu@unsa.edu.pe>
2024-06-26 20:12:06 -0500

    Agregando funcin velocidad y funcion para dibujar

--- CinematicCalculator.py
+++ CinematicCalculator.py
@@ -33,3 +33,37 @@ def tiempo_MRU(d, v):
 def distancia_MRUV(vi, t, a):
     check_negative(t)
     return vi * t + a * t ** 2 / 2
+
+def velocidad_MRUV(vi, t, a):
+    check_negative(t)
+    return vi + a * t
+
+def draw(vi, t, a):
+    check_zero(t)
+    check_negative(t)
+    paso = t / 10
+    tiempos = np.arange(0, t + paso, paso)
+    x = [distancia_MRUV(vi, i, a) for i in tiempos]
+    fig = Figure(figsize=(6, 4), dpi=100)
+    ax = fig.add_subplot(111)
+    ax.plot(tiempos, x)
+    ax.set_title("Δx en función del tiempo")
+    ax.set_xlabel("Tiempo (segundos)")
+    ax.set_ylabel("Distancia (metros)")
+
+    plot_frame = ttk.Frame(root)
+    plot_frame.grid(row=17, column=0, columnspan=2, padx=10, pady=10)
+
+    canvas = FigureCanvasTkAgg(fig, master=plot_frame)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+
+def calcular():
+    try:
+        if opcion.get() == "MRU":
+            tipo_calculo = tipo_calculo_mru.get()
+            if tipo_calculo == "Distancia (Δx)":
+                v = float(v_entry.get())
+                t = float(t_entry.get())
+                resultado = distancia_MRU(v, t)
+                messagebox.showinfo("Resultado", f"Distancia (Δx): {resultado:.2f} m")
